20c6bca2bd1c0227bd7014a85493509b320bbdb2
alexkar598 <>
2020-03-04 17:48:27 -0500

    Arguments are now better parsed.
    New option: -p/--parser to select the parser used to analyze the results, current options are 2019 for the 2019 vote and 2020 for the upcoming vote(format may change, this is a prerelease)
    
    New alias for -h, --help
    
    Refactor of the parser to allow more than a single parser

--- bin/main.js
+++ bin/main.js
@@ -12,27 +12,42 @@ const results = {};
 const candidates = [];
 const winners = [];
 const yargs_1 = __importDefault(require("yargs"));
+const path_1 = __importDefault(require("path"));
 const argsv = yargs_1.default
     .options({
     "f": {
         alias: "file",
         nargs: 1,
         description: "Uses a file",
-        demandOption: true
+        demandOption: true,
+        string: true
     },
     "v": {
         alias: "verbose",
-        description: "Shows additional information about the process"
+        description: "Shows additional information about the process",
+        boolean: true
     },
     "n": {
         alias: "number",
         description: "Number of candidates to elect",
         default: 4,
+        number: true
+    },
+    "p": {
+        alias: "parser",
+        description: "Parser version to use (defaults to latest version, used for historical recounts)",
+        default: "2019",
+        string: true,
+        choices: [
+            "2019",
+            "2020"
+        ]
     }
 })
     .usage("Usage: $0 -f <file> [options]")
     .example("$0 data.csv", "Parse the file data.csv")
     .help("h")
+    .alias("h", "help")
     .epilog("Program created by alexkar598 using Node.JS. This software is licensed under GPL and if you can read this message, you are allowed to access the source code at https://github.com/alexkar598/irvcounter")
     .argv;
 const verbose = argsv.v;
@@ -67,6 +82,10 @@ function parseResults() {
             for (const votes of Object.values(results)) {
                 let casted = false;
                 for (let x = 0; !casted; x++) {
+                    if (votes[x] === undefined) {
+                        console.vlog("Skipping ballot because end of list has been reached and no candidates have won");
+                        break;
+                    }
                     if (roundResults[votes[x]] !== undefined) {
                         roundResults[votes[x]]++;
                         casted = true;
@@ -121,33 +140,68 @@ function parseResults() {
     }
     console.log(`\n\nThe winners are: \n${winners.join("\n")}`);
 }
-// noinspection JSUnusedGlobalSymbols
-const transform = new stream_1.Stream.Transform({
-    transform: (chunk, encoding, callback) => {
-        chunk = buffer_replace_1.default(chunk, "\r\n,\r\n", "\n");
-        callback(null, buffer_replace_1.default(chunk, "\n,\n", "\n"));
+function readCsv(transform, oncb, mapvalcb) {
+    fs_1.default.createReadStream(path_1.default.resolve(process.cwd(), argsv.f))
+        .pipe(transform)
+        .pipe(csv_parser_1.default({
+        //@ts-ignore
+        mapValues: mapvalcb,
+        newline: "\n",
+        raw: true,
+    }))
+        .on("data", oncb)
+        .on("end", () => {
+        parseResults();
+    });
+}
+switch (argsv.p) {
+    case "2019": {
+        const transform = new stream_1.Stream.Transform({
+            transform: (chunk, encoding, callback) => {
+                chunk = buffer_replace_1.default(chunk, "\r\n,\r\n", "\n");
+                callback(null, buffer_replace_1.default(chunk, "\n,\n", "\n"));
+            }
+        });
+        readCsv(transform, ({ Vote, Voter }) => {
+            if (!results[Voter])
+                results[Voter] = [];
+            results[Voter].push(Vote.toString());
+        }, ({ value, header }) => {
+            value = value.toString().trim();
+            if (header.toString() === "Vote" && !candidates.includes(value))
+                candidates.push(value);
+            return value;
+        });
+        break;
     }
-});
-// @ts-ignore
-fs_1.default.createReadStream(argsv.f)
-    .pipe(transform)
-    .pipe(csv_parser_1.default({
-    //@ts-ignore
-    mapValues: ({ value, header }) => {
-        value = value.toString().trim();
-        if (header.toString() === "Vote" && !candidates.includes(value))
-            candidates.push(value);
-        return value;
-    },
-    newline: "\n",
-    raw: true,
-}))
-    .on("data", ({ Vote, Voter }) => {
-    if (!results[Voter])
-        results[Voter] = [];
-    results[Voter].push(Vote.toString());
-})
-    .on("end", () => {
-    parseResults();
-});
+    case "2020": {
+        const transform = new stream_1.Stream.Transform({
+            transform: (chunk, encoding, callback) => {
+                callback(null, buffer_replace_1.default(chunk, "\r\n", "\n"));
+            }
+        });
+        readCsv(transform, (args) => {
+            if (!args.Token) {
+                console.error("file format not supported, Token column is missing, are you using the right parser and is your file formatted correctly?");
+                process.exit(1);
+            }
+            if (!results[args.Token])
+                results[args.Token] = [];
+            for (const [key, value] of Object.entries(args)) {
+                const parsedKey = parseInt(key);
+                if (!isNaN(parsedKey)) {
+                    results[args.Token][parsedKey - 1] = value;
+                }
+            }
+        }, ({ value, header }) => {
+            value = value.toString().trim();
+            const parsedKey = parseInt(header.toString());
+            if (!isNaN(parsedKey)) {
+                if (!candidates.includes(value) && value !== "")
+                    candidates.push(value);
+            }
+            return value;
+        });
+    }
+}
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import fs from "fs"
 import csv from "csv-parser"
-import {Stream} from "stream"
+import {Stream, Transform} from "stream"
 import replace from "buffer-replace"
 
 const results: {
@@ -11,6 +11,7 @@ const candidates: Array<string> = []
 const winners: Array<string> = []
 
 import yargs from "yargs"
+import path from "path"
 
 const argsv = yargs
 	.options({
@@ -18,27 +19,42 @@ const argsv = yargs
 			alias: "file",
 			nargs: 1,
 			description: "Uses a file",
-			demandOption: true
+			demandOption: true,
+			string: true
 		},
 		"v": {
 			alias: "verbose",
-			description: "Shows additional information about the process"
+			description: "Shows additional information about the process",
+			boolean: true
 		},
 		"n": {
 			alias: "number",
 			description: "Number of candidates to elect",
 			default: 4,
+			number: true
+		},
+		"p": {
+			alias: "parser",
+			description: "Parser version to use (defaults to latest version, used for historical recounts)",
+			default: "2019",
+			string: true,
+			choices: [
+				"2019",
+				"2020"
+			]
 		}
 	})
 	.usage("Usage: $0 -f <file> [options]")
 	.example("$0 data.csv", "Parse the file data.csv")
 	.help("h")
+	.alias("h", "help")
 	.epilog("Program created by alexkar598 using Node.JS. This software is licensed under GPL and if you can read this message, you are allowed to access the source code at https://github.com/alexkar598/irvcounter")
 	.argv
 
 const verbose = argsv.v
 
 declare global {
+	// noinspection JSUnusedGlobalSymbols
 	interface Console {
 		// eslint-disable-next-line @typescript-eslint/no-explicit-any
 		vlog(...args: any[]): void;
@@ -49,8 +65,6 @@ declare global {
 console.vlog = function(...args): void {
 	if(verbose) console.log(...args)
 }
-
-
 
 function parseResults(): void {
 	const losers: {[key: string]: number} = {}
@@ -81,6 +95,10 @@ function parseResults(): void {
 			for (const votes of Object.values(results)) {
 				let casted = false
 				for(let x = 0; !casted; x++) {
+					if(votes[x] === undefined){
+						console.vlog("Skipping ballot because end of list has been reached and no candidates have won")
+						break
+					}
 					if (roundResults[votes[x]] !== undefined) {
 						roundResults[votes[x]]++
 						casted = true
@@ -136,35 +154,69 @@ function parseResults(): void {
 	}
 	console.log(`\n\nThe winners are: \n${winners.join("\n")}`)
 }
-// noinspection JSUnusedGlobalSymbols
-const
-	transform = new Stream.Transform({
-		transform: (chunk: Buffer, encoding, callback): void => {
-			chunk = replace(chunk, "\r\n,\r\n", "\n")
-			callback(null, replace(chunk, "\n,\n", "\n"))
-		}
-	})
 
-// @ts-ignore
-fs.createReadStream(argsv.f)
-	.pipe(transform)
-	.pipe(csv({
-		//@ts-ignore
-		mapValues: ({value, header}: { value: string; header: Buffer; index: number }) => {
+function readCsv(transform: Transform, oncb: (args: unknown) => void, mapvalcb?: ({value, header}) => unknown): void {
+	fs.createReadStream(path.resolve(process.cwd(), argsv.f))
+		.pipe(transform)
+		.pipe(csv({
+			//@ts-ignore
+			mapValues: mapvalcb,
+			newline: "\n",
+			raw: true,
+
+		}))
+		.on("data", oncb)
+		.on("end", () => {
+			parseResults()
+		})
+}
+
+switch(argsv.p) {
+	case "2019": {
+		const transform = new Stream.Transform({
+			transform: (chunk: Buffer, encoding, callback): void => {
+				chunk = replace(chunk, "\r\n,\r\n", "\n")
+				callback(null, replace(chunk, "\n,\n", "\n"))
+			}
+		})
+
+		readCsv(transform, ({Vote, Voter}: { Vote: string; Voter: string }) => {
+			if (!results[Voter]) results[Voter] = []
+
+			results[Voter].push(Vote.toString())
+		},({value, header}: { value: string; header: Buffer; index: number }) => {
 			value = value.toString().trim()
 			if (header.toString() === "Vote" && !candidates.includes(value)) candidates.push(value)
 			return value
-		},
-		newline: "\n",
-		raw: true,
+		})
 
-	}))
-	.on("data", ({Vote, Voter}: { Vote: string; Voter: string }) => {
-		if (!results[Voter]) results[Voter] = []
-
-		results[Voter].push(Vote.toString())
-	})
-	.on("end", () => {
-		parseResults()
-	})
-
+		break
+	}
+	case "2020":{
+		const transform = new Stream.Transform({
+			transform: (chunk: Buffer, encoding, callback): void => {
+				callback(null, replace(chunk, "\r\n", "\n"))
+			}
+		})
+		readCsv(transform, (args: {Token: string; [key: number]: string}) => {
+			if(!args.Token){
+				console.error("file format not supported, Token column is missing, are you using the right parser and is your file formatted correctly?")
+				process.exit(1)
+			}
+			if (!results[args.Token]) results[args.Token] = []
+			for(const [key, value] of Object.entries(args)){
+				const parsedKey = parseInt(key)
+				if(!isNaN(parsedKey)){
+					results[args.Token][parsedKey-1] = value
+				}
+			}
+		}, ({value, header}) => {
+			value = value.toString().trim()
+			const parsedKey = parseInt(header.toString())
+			if(!isNaN(parsedKey)){
+				if(!candidates.includes(value) && value !== "") candidates.push(value)
+			}
+			return value
+		})
+	}
+}
